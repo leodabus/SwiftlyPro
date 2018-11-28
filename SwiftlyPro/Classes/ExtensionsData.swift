71529953c98bf33a131222ddd4e47d0ba0d4a96b
Rashwan Lazkani <rashwanlazkani@Rashwans-MacBook-Pro.local>
2018-11-28 22:26:31 +0100

    Added documentation

--- SwiftlyPro/Classes/ExtensionsData.swift
+++ SwiftlyPro/Classes/ExtensionsData.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 public extension Data {
+    
+    /// Converts an HTML String to an NSAttributedString
     public var html2AttributedString: NSAttributedString? {
         do {
             return try NSAttributedString(data: self, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
@@ -15,6 +17,8 @@ public extension Data {
             return  nil
         }
     }
+    
+    /// Converts an HTML String to a String
     public var html2String: String {
         return html2AttributedString?.string ?? ""
     }
